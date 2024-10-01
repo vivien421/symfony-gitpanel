0b76fafb8d4e4d73db4b222c8cb50bb76abc5c46
vivien421 <vivien.boudin@hotmail.com>
2024-10-01 16:07:53 +0200

    Rename home by app and init TailwindCSS

--- importmap.php
+++ importmap.php
@@ -12,8 +12,8 @@
  * The "importmap:require" command can be used to add new entries to this file.
  */
 return [
-    'home' => [
-        'path' => './assets/home.js',
+    'app' => [
+        'path' => './assets/app.js',
         'entrypoint' => true,
     ],
 ];
